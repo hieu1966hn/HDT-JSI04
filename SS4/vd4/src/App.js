c412041ee9fca18e384c29106840031faaeb46b5
hieu1966hn <hieu1966hn@gmail.com>
2021-08-01 11:17:33 +0700

    new code

--- SS4/vd4/src/App.js
+++ SS4/vd4/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="_vd4">
-      
+      <Navbar />
     </div>
   );
 }
