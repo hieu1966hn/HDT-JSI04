63b837dbd43830a92a7a781b4f06f9dc80b902df
hieu1966hn <hieu1966hn@gmail.com>
2021-08-01 11:45:57 +0700

    new props

--- SS4/vd4/src/App.js
+++ SS4/vd4/src/App.js
@@ -2,11 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+import ContentSection from './components/ContentSection';
 
 function App() {
   return (
     <div className="_vd4">
       <Navbar />
+      <Header/>
+      <ContentSection reversed={true}/>
+      <ContentSection reversed={false}/>
+      <ContentSection reversed={true}/>
     </div>
   );
 }
