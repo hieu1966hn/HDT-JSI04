252fc79096ba4651c049aac863cac47bb6088616
hieu1966hn <hieu1966hn@gmail.com>
2021-08-01 11:54:29 +0700

    done bootStrap first page

--- SS4/vd4/src/App.js
+++ SS4/vd4/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import ContentSection from './components/ContentSection';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <ContentSection reversed={true} img={"assets/img/01.jpg"} />
       <ContentSection reversed={false} img={"assets/img/02.jpg"} />
       <ContentSection reversed={true} img={"assets/img/03.jpg"} />
+      <Footer />
     </div>
   );
 }
---
+++ SS4/vd4/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="py-5 bg-black">
+      <div className="container px-5">
+        <p className="m-0 text-center small">
+          Copyright © Your Website 2021
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
