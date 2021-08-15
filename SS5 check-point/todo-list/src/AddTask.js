45ecc854653a87cf1d7c24359590676222da206c
hieu1966hn <hieu1966hn@gmail.com>
2021-08-15 11:19:48 +0700

    done addTask

--- SS5 check-point/todo-list/src/AddTask.js
+++ SS5 check-point/todo-list/src/AddTask.js
@@ -18,6 +18,11 @@ class AddTask extends Component {
         this.linkList();
     }
 
+    isChangedName= (e)=>{
+        this.setState({
+            name: this.target.value
+        })
+    }
 
     render() {
         if (this.state.showTaskList == true) {
@@ -25,14 +30,30 @@ class AddTask extends Component {
                 <TaskList />
             )
         }
-        else{
+        else {
             <div className="container">
                 <h2>Add New Task</h2>
-                <div className="">
-
+                <div className="form-group">
+                    <label>Name</label>
+                    <input type="text"
+                        className="form-control"
+                        placeholder="Enter name of task"
+                        onChange={this.isChangedName}
+                    />
                 </div>
 
+                <buton type="submit"
+                    className="btn btn-default"
+                    onClick={this.hanlderAddTask}
+                >Add</buton>
+                <buton 
+                type="button" 
+                className="btn btn-default"
+                onClick={this.linkList}
+                >Back</buton>
             </div>
         }
     }
 }
+
+export default AddTask
