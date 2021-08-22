4c02991d966ba7bf32d50e499496c165c387288d
hieu1966hn <hieu1966hn@gmail.com>
2021-08-22 10:53:22 +0700

    new done

--- SS4/vd4/src/App.js
+++ SS4/vd4/src/App.js
@@ -1,19 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/Content/Content.css';
 
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import ContentSection from './components/ContentSection';
 import Footer from './components/Footer';
+import Content from './components/Content/Content.js';
 
 function App() {
   return (
     <div className="_vd4">
       <Navbar />
       <Header />
-      <ContentSection reversed={true} img={"assets/img/01.jpg"} />
-      <ContentSection reversed={false} img={"assets/img/02.jpg"} />
-      <ContentSection reversed={true} img={"assets/img/03.jpg"} />
+      <div className="container">
+
+        <div className="row pt-5">
+        <Content reversed={false} img={"assets/img/01.jpg"} />
+        <Content reversed={false} img={"assets/img/02.jpg"} />
+        <Content reversed={false} img={"assets/img/03.jpg"} />
+        </div>
+      </div>
       <Footer />
     </div>
   );
