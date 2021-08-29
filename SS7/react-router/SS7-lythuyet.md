d93c9fad064bedd9e8c383a9d3e1e08c7f1b11c3
hieu1966hn <hieu1966hn@gmail.com>
2021-08-29 11:27:47 +0700

    done App.js

---
+++ SS7/react-router/SS7-lythuyet.md
@@ -0,0 +1,9 @@
+Sơ lược về react-router:
+  - React.JS nó chỉ là thư viện để tạo các components phục vụ cho việc xây dựng giao diện.
+  - Vấn đề đặt ra là bản thân ReactJS không có Router. Vì vậy nó sinh ra react router để giải quyết vấn đề này
+  ==> Giúp đồng UI với URL.
+
+  Quy trình: 
+    1. Chọn router
+    2. Xây dựng định tuyến
+    3. Điều hướng giữa các tuyến sử dụng liên kết.
--- SS7/react-router/src/App.js
+++ SS7/react-router/src/App.js
@@ -1,13 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from "./components/Home"
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <div className="main-route-place">
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/topics" component={Topics} />
+          </div>
+
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Home</h2>
+      </div>
+    );
+  }
+}
+
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h2>About</h2>
+      </div>
+    );
+  }
+}
+
+
+class Topics extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Topics</h2>
+      </div>
+    );
+  }
+}
+
+
+
 export default App;
+
+
