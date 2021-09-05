4599e136f29b9d5ef785c492202153406e0f239e
hieu1966hn <hieu1966hn@gmail.com>
2021-09-05 11:52:11 +0700

    done dashboard

--- SS8/newplan/src/App.js
+++ SS8/newplan/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import Navbar from "./components/layout/Navbar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Dashboard from "./components/dashboard/Dashboard";
 
 
 class App extends Component {
@@ -9,6 +10,12 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
+
+          <Switch>
+            <Route path='/' component={Dashboard} />
+          </Switch>
+
+
         </div>
       </BrowserRouter>
     );
