3a7d6b912f14b75043945103a73910a8a69a466e
hieu1966hn <hieu1966hn@gmail.com>
2021-09-05 12:01:37 +0700

    done projectlist.js

---
+++ SS8/newplan/src/components/project/ProjectList.js
@@ -0,0 +1,39 @@
+
+
+const ProjectList = () => {
+  return (
+    <div className="project-list section">
+
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project title</span>
+          <p>Posted by Hieu PC</p>
+          <p>3rd Semtember, 11h58 am</p>
+        </div>
+      </div>
+
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project title</span>
+          <p>Posted by Hieu PC</p>
+          <p>3rd Semtember, 11h58 am</p>
+        </div>
+      </div>
+
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project title</span>
+          <p>Posted by Hieu PC</p>
+          <p>3rd Semtember, 11h58 am</p>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default ProjectList
