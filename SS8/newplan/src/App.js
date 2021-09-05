75ace095a8a5b0b998889e4aa5703034d1edffe0
hieu1966hn <hieu1966hn@gmail.com>
2021-09-05 10:50:48 +0700

    done newplan

---
+++ SS8/newplan/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>NewPlan</h1>
+      </div>
+    );
+  }
+
+}
+
+export default App;
