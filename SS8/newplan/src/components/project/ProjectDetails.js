235f53311704f7a3c56a17084cabdc2d989e2398
hieu1966hn <hieu1966hn@gmail.com>
2021-09-12 10:45:16 +0700

    done

--- SS8/newplan/src/components/project/ProjectDetails.js
+++ SS8/newplan/src/components/project/ProjectDetails.js
@@ -3,9 +3,17 @@ const ProjectDetails = () => {
     <div className="container section project-details">
       <div className="card z-depth-0">
         <div className="card-content">
-          <span className="card-title">Project title - { }</span>
+          <span className="card-title">Project title </span>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, assumenda nemo nulla suscipit exercitationem reiciendis veritatis fugiat. Adipisci quidem quibusdam blanditiis magnam itaque odit nam voluptates vel velit hic? Quis!</p>
+        </div>
+
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by NTH</div>
+          <div>12 september, 10:44 am</div>
         </div>
       </div>
     </div>
   )
 }
+
+export default ProjectDetails;
