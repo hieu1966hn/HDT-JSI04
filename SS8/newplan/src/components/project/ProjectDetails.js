8e67abb0da698aea86ea0ae24d1796eb73c13159
hieu1966hn <hieu1966hn@gmail.com>
2021-09-12 10:44:57 +0700

    done project detail

---
+++ SS8/newplan/src/components/project/ProjectDetails.js
@@ -0,0 +1,11 @@
+const ProjectDetails = () => {
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project title - { }</span>
+        </div>
+      </div>
+    </div>
+  )
+}
