a72e219e1973ac37eaae458f528fe17c200f82ff
hieu1966hn <hieu1966hn@gmail.com>
2021-09-12 11:42:57 +0700

    done signup

--- SS8/newplan/src/components/auth/SignUp.js
+++ SS8/newplan/src/components/auth/SignUp.js
@@ -3,15 +3,37 @@ import { Component } from "react";
 
 // lam trang dang nhap
 class SignUp extends Component {
+  state = {
+    email: "",
+    password: '',
+    firstName: "",
+    lastName: ''
+  }
+  // khai bao bien state chay ngam ben trong react
+
+
+  // khoi tao phuong thuc hanldeChange
+  handleChange = (e) => {
+    this.setState = ({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  // khoi tao phuong thuc handleSubmit
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="container">
-        <form className="white">
+        <form className="white" onSubmit={this.handleSubmit}>
           <h5 className="grey-text text-darken-3">Sign Up</h5>
 
           <div className="input-field">
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
+            <input type="email" id="email" onChange={this.handleChange} />
           </div>
 
           <div className="input-field">
