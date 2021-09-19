8b580756fc249007db3f4ec98efd14763d7738bb
hieu1966hn <hieu1966hn@gmail.com>
2021-09-19 11:08:03 +0700

    done Dasshboard

--- SS8/newplan/src/components/dashboard/Dashboard.js
+++ SS8/newplan/src/components/dashboard/Dashboard.js
@@ -1,9 +1,11 @@
 import { Component } from "react";
 import ProjectList from "../project/ProjectList";
-import Notifications from "./Notifications"
+import Notifications from "./Notifications";
+import { connect } from "react-redux"
 
 class Dashboard extends Component {
   render() {
+    console.log(this.props);
     return (
       <div className="dashboard container">
         <div className="row">
@@ -25,4 +27,10 @@ class Dashboard extends Component {
 
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard);
