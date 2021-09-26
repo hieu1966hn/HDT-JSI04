3ee747eb67c13615eff243a763a3292d94fd61f7
hieu1966hn <hieu1966hn@gmail.com>
2021-09-26 11:40:36 +0700

    done /create

--- SS8/newplan/src/App.js
+++ SS8/newplan/src/App.js
@@ -7,6 +7,8 @@ import ProjectDetails from "./components/project/ProjectDetails";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
 
+import CreateProject from "./components/project/CreateProject";
+
 
 class App extends Component {
   render() {
@@ -20,6 +22,7 @@ class App extends Component {
             <Route path="/project/:id" component={ProjectDetails} />
             <Route path="/signin" component={SignIn} />
             <Route path="/signup" component={SignUp} />
+            <Route path="/create" component={CreateProject} />
           </Switch>
 
         </div>
